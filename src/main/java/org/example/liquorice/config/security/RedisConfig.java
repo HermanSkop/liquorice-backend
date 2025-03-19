1cbddf380dce9350ce1ae47830caa58e0fb0b260
HermanSkop <armaton111@gmail.com>
2025-03-19 11:50:52 +0100

    Docker-friendly configuration added

--- src/main/java/org/example/liquorice/config/security/RedisConfig.java
+++ src/main/java/org/example/liquorice/config/security/RedisConfig.java
@@ -1,5 +1,6 @@
 package org.example.liquorice.config.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -9,9 +10,15 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
     @Bean
